3db5f8d804da903611d7bfe68ec620524299d9b4
Nguyen Tung Lam <lamnthnue.edu.vn@gmail.com>
2019-11-04 23:46:05 +0700

    'ok'

---
+++ src/app/pet/pet.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pet',
+  templateUrl: './pet.component.html',
+  styleUrls: ['./pet.component.scss']
+})
+export class PetComponent implements OnInit {
+  petName = 'puppie';
+  petImage = 'http://yourdost-blog-images.s3-ap-southeast-1.amazonaws.com/wp-content/uploads/2016/01/03165939/Dogs.jpg';
+
+
+  petName1 = 'puppie';
+  petImage1 = 'http://yourdost-blog-images.s3-ap-southeast-1.amazonaws.com/wp-content/uploads/2016/01/03165939/Dogs.jpg';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
